3a99f428e2153758fbb9aca08917c5e050dcc4cd
Regata OS Project <regataosproject@gmail.com>
2022-03-05 00:01:56 -0300

    Fix for hardware and software information detection.

--- opt/regataos-prime/scripts/detect-system-info.sh
+++ opt/regataos-prime/scripts/detect-system-info.sh
@@ -3,42 +3,44 @@
 sleep 3;
 
 # Collect hardware information
-if test ! -e "$HOME/.config/regataos-prime/system-info"; then
-    mkdir -p "$HOME/.config/regataos-prime/system-info/"
+user=$(users | awk '{print $1}')
+
+if test ! -e "/home/$user/.config/regataos-prime/system-info"; then
+    mkdir -p "/home/$user/.config/regataos-prime/system-info/"
 fi
 
 #Capture Operational System
-echo $(grep -r PRETTY_NAME /etc/os-release | cut -d= -f 2 | sed 's/"//g') > "$HOME/.config/regataos-prime/system-info/os-name.txt"
+echo $(grep -r PRETTY_NAME /etc/os-release | cut -d= -f 2 | sed 's/"//g') > "/home/$user/.config/regataos-prime/system-info/os-name.txt"
 
 #Capture total amount of RAM
-echo $(free -h | grep -i Mem | awk '{print $2}' | sed 's/Mi/MB/' | sed 's/Gi/GB/') > "$HOME/.config/regataos-prime/system-info/ram-total.txt"
+echo $(free -h | grep -i Mem | awk '{print $2}' | sed 's/Mi/MB/' | sed 's/Gi/GB/') > "/home/$user/.config/regataos-prime/system-info/ram-total.txt"
 
 #Capture the CPU model
-echo $(lscpu | grep -i model | tail -n 1 | cut -d':' -f 2 | sed 's/^ \+//') > "$HOME/.config/regataos-prime/system-info/cpu-model.txt"
+echo $(lscpu | grep -i model | tail -n 1 | cut -d':' -f 2 | sed 's/^ \+//') > "/home/$user/.config/regataos-prime/system-info/cpu-model.txt"
 
 #Capture NVIDIA driver version
-echo $(nvidia-smi -i 0 --query-gpu=driver_version --format=csv,noheader) > "$HOME/.config/regataos-prime/system-info/nvdriver-version.txt"
+echo $(nvidia-smi -i 0 --query-gpu=driver_version --format=csv,noheader) > "/home/$user/.config/regataos-prime/system-info/nvdriver-version.txt"
 
 #Capture the discrete GPU video memory (VRAM) size
-echo $(/bin/bash /opt/regataos-prime/scripts/hardware-info -vram-size) > "$HOME/.config/regataos-prime/system-info/vram-size.txt"
+echo $(/bin/bash /opt/regataos-prime/scripts/hardware-info -vram-size) > "/home/$user/.config/regataos-prime/system-info/vram-size.txt"
 
 #Capture iGPU
-echo $(/bin/bash /opt/regataos-prime/scripts/hardware-info -capture-igpu) > "$HOME/.config/regataos-prime/system-info/igpu-model.txt"
+echo $(/bin/bash /opt/regataos-prime/scripts/hardware-info -capture-igpu) > "/home/$user/.config/regataos-prime/system-info/igpu-model.txt"
 
 #Capture dgpu
-echo $(/bin/bash /opt/regataos-prime/scripts/hardware-info -capture-dgpu) > "$HOME/.config/regataos-prime/system-info/dgpu-model.txt"
+echo $(/bin/bash /opt/regataos-prime/scripts/hardware-info -capture-dgpu) > "/home/$user/.config/regataos-prime/system-info/dgpu-model.txt"
 
 #Capture MESA version
-echo $(/bin/bash /opt/regataos-prime/scripts/hardware-info -mesa-version) > "$HOME/.config/regataos-prime/system-info/mesa-version.txt"
+echo $(/bin/bash /opt/regataos-prime/scripts/hardware-info -mesa-version) > "/home/$user/.config/regataos-prime/system-info/mesa-version.txt"
 
 #Capture OpenGL version
-echo $(/bin/bash /opt/regataos-prime/scripts/hardware-info -opengl-version) > "$HOME/.config/regataos-prime/system-info/opengl-version.txt"
+echo $(/bin/bash /opt/regataos-prime/scripts/hardware-info -opengl-version) > "/home/$user/.config/regataos-prime/system-info/opengl-version.txt"
 
 #Capture Vulkan version
 /bin/bash /opt/regataos-prime/scripts/hardware-info -vulkan-version
-echo $(cat "/tmp/regataos-prime/vulkan-version.txt" | awk '{print $4}') > "$HOME/.config/regataos-prime/system-info/vulkan-version.txt"
+echo $(cat "/tmp/regataos-prime/vulkan-version.txt" | awk '{print $4}') > "/home/$user/.config/regataos-prime/system-info/vulkan-version.txt"
 
 #Capture Kernel version
-echo $(/bin/bash /opt/regataos-prime/scripts/hardware-info -kernel-version) > "$HOME/.config/regataos-prime/system-info/kernel-version.txt"
+echo $(/bin/bash /opt/regataos-prime/scripts/hardware-info -kernel-version) > "/home/$user/.config/regataos-prime/system-info/kernel-version.txt"
 
 exit;
