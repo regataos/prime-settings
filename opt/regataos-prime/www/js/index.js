485522b1aae5a61beccbcbc6b3f8801f61a9cd5f
Josué Vicente <josue2363@gmail.com>
2024-02-24 15:30:44 -0300

    Open the app window centered on the screen.

--- opt/regataos-prime/www/js/index.js
+++ opt/regataos-prime/www/js/index.js
@@ -4,6 +4,12 @@ onload = function () {
     gui.Window.get().show();
 }
 
+const win = nw.Window.get();
+// Move window to top left of screen
+win.moveTo(0, 0);
+// Move window to middle of screen
+win.setPosition('center');
+
 // Disable main hover effect after few seconds
 setTimeout(function () {
     document.getElementById("loadscreen").style.display = "none";
